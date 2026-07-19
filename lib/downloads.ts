1ae03965dea9c93b6a469e199a6f306b0e561974
Jere <jere.arata@gmail.com>
2026-07-18 22:36:38 -0300

    Release v1.5.0: finalize Windows installer support

--- lib/downloads.ts
+++ lib/downloads.ts
@@ -15,7 +15,7 @@ export type DownloadPlatform = keyof typeof downloads;
 
 /** Stable download routes. Adding the installer files requires no component changes. */
 export const ANDROID_DOWNLOAD_URL = "https://github.com/eljerexd/Web-SuelosAR/releases/latest/download/SuelosAR-Android.apk";
-export const WINDOWS_DOWNLOAD_URL = downloads.windows.route;
+export const WINDOWS_DOWNLOAD_URL = "https://github.com/eljerexd/Web-SuelosAR/releases/latest/download/SuelosAR-Setup.exe";
 
 export function isDownloadPlatform(value: string): value is DownloadPlatform {
   return value in downloads;
